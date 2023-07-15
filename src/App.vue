d89a666544ea408b71dcf0be23cd5cc63c2e306d
jingrui <790150796@qq.com>
2023-07-15 16:17:17 +0800

    feat: add a h1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="box">
     <h1>我是App</h1>
+    <h1>新增一个标题</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-const fun = () => 
-{
-  console.log("888");
+const fun = () => {
+  console.log('888')
 }
-fun();
+fun()
 </script>
 
 <style scoped lang="scss">
-.box{
+.box {
   height: 100px;
 }
 </style>
